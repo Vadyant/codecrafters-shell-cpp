9078886e9775c5906cca2bee5b9913082e27a12a
Vadyant <vadayantbhidolia1974@gmail.com>
2025-12-29 20:02:20 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,79 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 string line;
 bool active = true;
-vector<string> builtin={"echo","exit","type"};
-vector<string>word;
+vector<string> builtin = {"echo", "exit", "type"};
+vector<string> word;
 int len;
 string val;
 
-void print(int a,int b){
-  for(int i=a;i<b-1;i++){
-    cout<<word[i]<<' ';
+void print(int a, int b) {
+  if (a >= b) {
+    cout << endl;
+    return;
   }
-  cout<<word[b-1]<<val<<endl;
+  for (int i = a; i < b - 1; i++) {
+    cout << word[i] << ' ';
+  }
+  cout << word[b - 1] << val << endl;
 }
 
-void read(){
+void read() {
   word.clear();
   cout << "$ ";
-  getline(cin,line);
-  string temp="";
+  getline(cin, line);
 
-  for(int i=0;i<line.length();i++){
-    if(line[i]==' '){
-      word.push_back(temp);
-      temp="";
-    }
-    else temp+=line[i];
+  string temp = "";
+  for (char c : line) {
+    if (c == ' ') {
+      if (!temp.empty()) word.push_back(temp);
+      temp = "";
+    } else temp += c;
   }
-  word.push_back(temp);
-  int len=word.size();
+  if (!temp.empty()) word.push_back(temp);
+
+  len = word.size();
 }
 
-void eval(){
-  if(word[0]=="exit"){
-    active=false;
+void eval() {
+  if (word.empty()) return;
+
+  if (word[0] == "exit") {
+    active = false;
     return;
   }
-  
-  if(word[0]=="echo"){
-    val="";
-    print(1,len);
+
+  if (word[0] == "echo") {
+    val = "";
+    print(1, len);
     return;
   }
-  
-  if(word[0]=="type"){
-    auto it = find(builtin.begin(), builtin.end(), word[1]);
-    if(it != builtin.end()){
-      cout<<word[1]<<" is a shell builtin"<<endl;
+
+  if (word[0] == "type") {
+    if (len < 2) {
+      cout << "type: missing argument" << endl;
       return;
     }
-    val=": not found";
-    print(1,len);
+    auto it = find(builtin.begin(), builtin.end(), word[1]);
+    if (it != builtin.end()) {
+      cout << word[1] << " is a shell builtin" << endl;
+      return;
+    }
+    val = ": not found";
+    print(1, len);
     return;
   }
-  
-  cout<<line<<": command not found"<<endl;
-  
+
+  cout << line << ": command not found" << endl;
 }
 
 int main() {
-  // Flush after every std::cout / std:cerr
   cout << std::unitbuf;
   cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  while (active) {  
+  while (active) {
     read();
     eval();
   }
-  return 0;
 }
