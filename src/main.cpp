c1ddb205c81f3f72382ab3408364374574c3253c
vadyant bhudolia <vadyant.bhudolia.ece25@itbhu.ac.in>
2026-01-03 15:10:47 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -97,22 +97,16 @@ void eval() {
     }
   }
   // not builtin
-  for(auto d:directory){
-    d=d+"/"+word[0];
-    if(access(d.c_str(),X_OK)==0){
+  int pid = fork();
 
-      int pid = fork();
-    
-      if (pid==0){
-        execv(d.c_str(), argv.data());
-        perror("execvp");
-        exit(1);
-      }else if(pid>0){
-        wait(NULL);
-      }
-      return;
-    }
+  if (pid == 0) {
+    execvp(argv[0], argv.data());
+    perror(argv[0]);   // only if execvp fails
+    exit(127);
+  } else if (pid > 0) {
+    waitpid(pid, nullptr, 0);
   }
+
 
 }
 
